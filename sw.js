1ea0d978a953c61e170fe2d78106f6bd482a8ae8
wbnjzmcsgy-max <wbnjzmcsgy@privaterelay.appleid.com>
2026-09-24 01:18:56 +0900

    Refresh PWA cache for datetime layout fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='call-log-v2',A=['./','./index.html','./manifest.json'];
+const C='call-log-v3',A=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
